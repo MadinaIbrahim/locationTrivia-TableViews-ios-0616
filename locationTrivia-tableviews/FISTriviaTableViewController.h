9c5c78aad2a2eeca623b74b15c4af5ab5b772b23
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-06-25 17:07:53 -0400

    Done.

---
+++ locationTrivia-tableviews/FISTriviaTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FISTriviaTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Madina Ibrahim on 6/25/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISTrivium.h"
+#import "FISLocation.h"
+
+@interface FISTriviaTableViewController : UITableViewController
+
+@property(strong, nonatomic)NSArray *triviaArray;
+
+@end
